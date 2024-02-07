2f1e5a9d846fcd725ded8522ab66466d162da75f
Samrat Bajracharya <siddhibro@gmail.com>
2024-02-07 14:00:19 +0545

    category and books model created

--- Book/books/models.py
+++ Book/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
+class Books(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ManyToManyField(Category)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
