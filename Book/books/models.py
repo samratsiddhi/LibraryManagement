e1f78edd78053423d765c243c1c697976172df95
Samrat Bajracharya <siddhibro@gmail.com>
2024-02-08 10:50:31 +0545

    books model created

--- Book/books/models.py
+++ Book/books/models.py
@@ -10,6 +10,8 @@ class Category(models.Model):
 class Books(models.Model):
     name = models.CharField(max_length=255)
     category = models.ManyToManyField(Category)
+    created_at = models.TimeField(auto_now_add=True)
+    quantity = models.IntegerField()
     
     def __str__(self) -> str:
         return self.name
