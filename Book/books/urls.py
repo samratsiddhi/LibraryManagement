c2c14122b6cf42dc3e1b1cbfc218f9dfdcc12f38
Samrat Bajracharya <siddhibro@gmail.com>
2024-02-08 11:07:57 +0545

    urls included

--- Book/books/urls.py
+++ Book/books/urls.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from .views import CategoryViewSet,BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("category",CategoryViewSet,basename="category")
+router.register("books",BookViewSet,basename="books")
 
 urlpatterns = [
-]
+]+router.urls
