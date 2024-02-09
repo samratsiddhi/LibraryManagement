b1a105e288a8e945d65161fa84f174cc0bb1a372
Samrat Bajracharya <siddhibro@gmail.com>
2024-02-09 10:50:32 +0545

    publisher created

--- Book/books/publisher.py
+++ Book/books/publisher.py
@@ -3,12 +3,18 @@ from decouple import config
 
 connection = pika.BlockingConnection(pika.URLParameters(config('RABBITMQ')))
 channel = connection.channel()
+channel.queue_declare(queue= "borrow_book")
 
 def publish(message):
     try:
+        channel.queue_declare(queue= "borrow_book")
+
+        channel.basic_publish(exchange="",
+                              routing_key="borrow_book",
+                              body=message)
         print("connection successful")
         connection.close()
     except Exception as e:
         print(e)
         
-    print("message=", message)
+    print("message sent =  ", message)
